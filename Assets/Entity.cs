8bd9f4b721f0a21f7f5b822cb33da8143708e39a
Caiu-2000 <totaroezequiel2004@gmail.com>
2026-07-22 18:40:24 -0300

    punto de control

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -1,9 +1,99 @@
+
 using UnityEngine;
 
-
-[System.Serializable]
-public abstract class Entity
+public abstract class Entity : MonoBehaviour, IHittable
 {
-    [SerializeField] MovementComponent movementComponent;
 
+    [SerializeField] protected float _currentLife = 0, _maxLife = 100;
+
+    public bool _damCD = false;
+    public bool _CanInputMovement = true;
+
+    public float _maxStamina = 100.0f, _currentStamina = 0.0f;
+    public float _StaminaCD = 1f, _StaminaCount = 0, _StaminaRegen = 25f;
+
+    public delegate void HealthChange(float NewHealth, float MaxHealth);
+    public HealthChange OnHealthChanged = delegate { };
+
+    public delegate void Damaged();
+    public Damaged OnDamaged = delegate { };
+
+    public delegate void Dead();
+    public delegate void Attack();
+
+    public Dead OnEntityDead = delegate { };
+    public Attack OnEntityAttacked = delegate { };
+
+
+    private void Awake()
+    {
+        _currentLife = _maxLife;
+        _currentStamina = _maxStamina;
+        
+    }
+
+    public virtual void applyDamage(float damage)
+    {
+
+        if (_currentLife == 0) _currentLife = _maxLife;
+
+        OnDamaged?.Invoke();
+        if (_currentLife <= 0)
+        {
+            Die();
+        }
+
+
+    }
+
+    private void Update()
+    {
+        if (_StaminaCount > 0)
+        {
+            _StaminaCount -= Time.deltaTime;
+        }
+        else if (_currentStamina < _maxStamina)
+        {
+            _currentStamina += _StaminaRegen * Time.deltaTime;
+            if (_currentStamina > _maxStamina) _currentStamina = _maxStamina;
+        }
+    }
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+    public virtual void ReduceStamina(float Cost)
+    {
+        _currentStamina -= Cost;
+        if (_currentStamina < 0) _currentStamina = 0;
+    }
+
+    public virtual void Heal(float _healAmount)
+    {
+        _currentLife += _healAmount;
+        if (_currentLife > _maxLife) _currentLife = _maxLife;
+    }
+
+
+    public void SetShield(float amount)
+    {
+        currentShieldedLife = amount;
+    }
+    public void AddArmor(float amount)
+    {
+        _armor += amount;
+    }
+
+    public void RemoveArmor(float amount)
+    {
+        _armor = Mathf.Max(0, _armor - amount); // can't go below 0
+    }
+
+    void IHittable.Hitt(Hitt hitt)
+    {
+        applyDamage(hitt.HittDamage);
+    }
 }
--- Assets/MovementComponent.cs
+++ Assets/MovementComponent.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 
-public class MovementComponent : MonoBehaviour
+[System.Serializable]
+public class MovementComponent 
 {
+    [SerializeField] private Rigidbody _RB;
+    public float Speed = 3.0f;
 
+    public void Move(Vector2 moveDir)
+    {
+        _RB.linearVelocity = moveDir * Time.deltaTime * Speed;
+    }
 }
