a2c7e3f0dee64eda7e0ed6de3d16ef7404302c68
Caiu-2000 <totaroezequiel2004@gmail.com>
2026-07-26 10:40:32 -0300

    Reapply "Reapply "levels""
    This reverts commit cbafa71c605eba9970348afc8c8ae04a720ea83d.

--- Assets/Treekiller.cs
+++ Assets/Treekiller.cs
@@ -4,6 +4,7 @@ public class Treekiller : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<Tree>(out Tree tree)) { Destroy(tree.gameObject); }
+        if (other.TryGetComponent<Tree>(out Tree tree)) { Destroy(tree.
+            gameObject); }
     }
 }
