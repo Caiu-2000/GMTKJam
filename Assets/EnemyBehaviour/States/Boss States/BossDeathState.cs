825ec61372f3080e8fe577a4d0ecc0d11da4abfa
Caiu-2000 <totaroezequiel2004@gmail.com>
2026-07-26 10:37:29 -0300

    Reapply "levels"
    This reverts commit 554cc593ba35c924e6232ffa381ad3e934f503a8.

---
+++ Assets/EnemyBehaviour/States/Boss States/BossDeathState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossDeathState : DeathState
+{
+    public override void StartState()
+    {
+        ParentMachine._movement.Move(new Vector2(0, 0));
+
+    }
+
+    private IEnumerator DiadSecuence()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(3);
+    }
+}
