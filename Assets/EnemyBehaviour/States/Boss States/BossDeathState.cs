10b3dca29a56cd641bd473423b8bef99f901e597
Caiu-2000 <totaroezequiel2004@gmail.com>
2026-07-26 10:52:34 -0300

    Habemus Victoria

--- Assets/EnemyBehaviour/States/Boss States/BossDeathState.cs
+++ Assets/EnemyBehaviour/States/Boss States/BossDeathState.cs
@@ -7,12 +7,12 @@ public class BossDeathState : DeathState
     public override void StartState()
     {
         ParentMachine._movement.Move(new Vector2(0, 0));
-
+        StartCoroutine(DiadSecuence());
     }
 
     private IEnumerator DiadSecuence()
     {
         yield return new WaitForSeconds(2.0f);
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
     }
 }
