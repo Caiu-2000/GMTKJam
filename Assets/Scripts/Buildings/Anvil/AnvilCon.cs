0ef6c4c5029f7cac3d70224bdde1aea0275dbfa8
Caiu-2000 <132226432+Caiu-2000@users.noreply.github.com>
2026-08-20 20:46:50 -0300

    Fixed Uis

--- Assets/Scripts/Buildings/Anvil/AnvilCon.cs
+++ Assets/Scripts/Buildings/Anvil/AnvilCon.cs
@@ -9,7 +9,6 @@ public class AnvilCon : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        print("interact called");
         Player player = GeneralHandler.Instance.GetPlayer();
         if (panel.activeSelf == true) return;
         if(player.inventory.GetGold()>= 5) panel.SetActive(true);
